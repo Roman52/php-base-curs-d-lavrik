8899f661fbebefc61cea4b48197c4bdc246895f8
Roman Zharov <zharov.dev@gmail.com>
2022-03-05 21:12:05 +0100

    lesson 7

---
+++ 07-07/init.php
@@ -0,0 +1,13 @@
+<?php
+const HOST = 'http://localhost:8080';
+const BASE_URL = '/php1/07-07/';
+
+const DB_HOST = 'localhost';
+const DB_NAME = 'php1simple';
+const DB_USER = 'root';
+const DB_PASS = '';
+
+include_once ('core/arr.php');
+include_once ('core/db.php');
+include_once ('core/system.php');
+include_once ('model/messages.php');
---
+++ 07-07/views/contacts/v_menu.php
@@ -0,0 +1,5 @@
+<ul class="list-group">
+	<li class="list-group-item">Phone</li>
+	<li class="list-group-item">Email</li>
+	<li class="list-group-item">Form</li>
+</ul>
